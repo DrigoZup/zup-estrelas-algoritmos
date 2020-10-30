d0d2967678cd2f4080e838b9233b1cb4c3cea4bb
Drigo Zup <rodrigo.lima@zup.com.br>
2020-10-30 16:09:58 -0300

    Algoritmo de Busca

--- src/main/java/br/com/zup/estrelas/principal/Principal.java
+++ src/main/java/br/com/zup/estrelas/principal/Principal.java
@@ -1,28 +1,57 @@
 package br.com.zup.estrelas.principal;
 
+import java.util.Scanner;
+
 public class Principal {
 
-	public static void main(String[] args) {
-		
-		
-		int [] algoritmo = {2, 5, 1, 95, 54, 33, 71, 115, 7, 9, 4 };
-		int auxiliar = 0;
+	public static void ordenaArray(int[] array) {
 
-		for (int i = 0; i < algoritmo.length; i++) {
-			for (int j = 0;  j < algoritmo.length; j++) {
-				
-				if (algoritmo[i] < algoritmo[j]) {
-					auxiliar = algoritmo[i];    
-	                algoritmo[i] = algoritmo[j];    
-	                 algoritmo[j] = auxiliar;    
+		int auxiliar = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+
+				if (array[i] < array[j]) {
+					auxiliar = array[i];
+					array[i] = array[j];
+					array[j] = auxiliar;
 				}
 			}
 		}
-		
-		
-		for (int alg : algoritmo) {
-			
-			System.out.println(alg);
+	}
+
+	public static int buscaNumeroArray(int[] array, int numeroProcurado) {
+		for (int i = 0; i < array.length; i++) {
+
+			if (array[i] == numeroProcurado) {
+				return i;
+			}
 		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int[] algoritmo = { 2, 5, 1, 95, 54, 33, 71, 115, 7, 9, 4 };
+
+		ordenaArray(algoritmo);
+
+		for (int alg : algoritmo) {
+			System.out.print(alg + " ");
+		}
+
+		System.out.println("\n\nDigite um número a ser buscado na lista: ");
+		int numeroBuscado = scanner.nextInt();
+		
+		int resultadoBusca = buscaNumeroArray(algoritmo, numeroBuscado);
+		
+		if (resultadoBusca == -1) {
+			System.out.println("Número não encontrado");
+		} else {
+			System.out.println("A posição do número "+numeroBuscado+"\n"
+					+ "é a posição"+resultadoBusca);
+		}
+		
+		scanner.close();
 	}
 }
